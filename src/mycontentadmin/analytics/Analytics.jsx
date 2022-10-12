fa4dabfb126ebb8d7b51fd7d8e4a462a3c9fd929
karloslazaroo <karloplazaro@yahoo.com>
2022-10-12 22:11:23 +0800

    Update Analytics.jsx

--- src/mycontentadmin/analytics/Analytics.jsx
+++ src/mycontentadmin/analytics/Analytics.jsx
@@ -5,6 +5,7 @@ const Analytics = () => {
     <div className='analytics_body'>
        <div className="textBox">
       <h2>Analytics<br></br></h2>
+      <div className="divider_analytics"></div>
       </div>
     </div>
   )
