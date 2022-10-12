4a2ebffb1877b0b4802313fdfee890f26f752ebd
karloslazaroo <karloplazaro@yahoo.com>
2022-10-12 16:02:24 +0800

    huhu

--- src/mysuperadmin/homepage/Homepage.jsx
+++ src/mysuperadmin/homepage/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
         
   
     <div className="content">
-        <div className="textBox_admin">
+        <div className="textBox_superadmin">
             <h2>HELLO, <br></br> <span>{user?.displayName}</span></h2>
             <p>Super Administrator / College of Information and Computing Sciences</p><br></br>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores reprehenderit, soluta quae magnam quas quos dignissimos totam, sequi cum iure fuga dolor placeat ipsum nisi consequuntur sit magni, ex deleniti?</p>
