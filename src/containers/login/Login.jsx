b88eb56eca571260c82d49656d26c24e69387dad
karloslazaroo <karloplazaro@yahoo.com>
2022-10-18 17:07:47 +0800

    Update Login.jsx

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -36,7 +36,7 @@ function Signin (){
       const email = user.email;
       
     
-      if(email == "1karlosandrew.lazaro.iics@ust.edu.ph"){
+      if(email == "karlosandrew.lazaro.iics@ust.edu.ph"){
           navigate('/super');
       } else  {
         Axios.get(`http://localhost:3001/readAdminLogin/${email}`).then((response) => {
