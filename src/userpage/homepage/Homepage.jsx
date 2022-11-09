f0d8cd222bd6baec167766820b5d7f16e184b031
RomeRosal <geromeeleubert.rosal.cics@ust.edu.ph>
2022-11-09 20:21:37 +0800

    Update Homepage.jsx

--- src/userpage/homepage/Homepage.jsx
+++ src/userpage/homepage/Homepage.jsx
@@ -16,7 +16,7 @@ const Homepage = () => {
             <h2>Hello, <br></br> <span>{user?.displayName}</span></h2>
             <p>4ITD / College of Information and Computing Sciences</p><br></br>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores reprehenderit, soluta quae magnam quas quos dignissimos totam, sequi cum iure fuga dolor placeat ipsum nisi consequuntur sit magni, ex deleniti?</p>
-            <a href='/Chatbot'>Try out our Chatbot!</a>
+            <a href='/Talktous_User'>Message Us Now!</a>
             </div>
             <div class="imgBox">
             <img src= {img1} className='school'></img>
