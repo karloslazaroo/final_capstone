e5faefb100c06ab900ee6a19962dde579105ab0b
karloslazaroo <karloplazaro@yahoo.com>
2022-11-12 00:27:15 +0800

    hello

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -54,7 +54,7 @@ function Signin (){
       const email = user.email;
       
       
-      if(email == "karlosandrew.lazaro.cics@ust.edu.ph") {
+      if(email == "yehudijascha.ballesteros.cics@ust.edu.ph") {
           navigate('/super');
           // logthelogin("super");
       } else  {
